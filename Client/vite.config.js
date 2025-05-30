21a1362c653803dbebb8ac8f50715e303bec08af
Aarushi Rawat <aarushirawat43@gmail.com>
2025-05-30 22:56:59 +0530

    fixed host

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    allowedHosts: [
+      'chat-application-fiuv.onrender.com',
+      'another-host.com'
+    ]
+  },
   plugins: [react()],
 })
