7422f644aafeb352956c0324384e6c7be5bf22f0
Aarushi Rawat <aarushirawat43@gmail.com>
2025-05-31 00:40:18 +0530

    updating mongodb settings

--- server/models/Chat.model.js
+++ server/models/Chat.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const chatSchema = new mongoose.Schema({
   username: { type: String, required: true },
   message: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now, expires: 60 * 60 * 24 } // 24 hours in seconds
+  timestamp: { type: Date, default: Date.now, expires: 60 } // 24 hours in seconds
 });
 
 module.exports = mongoose.model('Chat', chatSchema);
